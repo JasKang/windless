0575d485f286f0cf2993dd4162eaf9d50d0ecb64
JasKang <jaskang@qq.com>
2024-08-19 22:40:20 +0800

    feat: headlessui

--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -1,52 +1,66 @@
-import type { FC } from 'react'
+import { type FC, useMemo } from 'react'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, type ListboxProps } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
 import { clsx } from 'kotl'
 import useValue from '@/hooks/useValue'
-import { InputBox } from '@/Input'
-import { Listbox } from '@/Listbox'
-import { Popper } from '@/Popper'
 
-type SelectProps = {
-  value?: string
-  onChange?: (value: string) => void
-  defaultValue?: string
-  options: string[]
-  disabled?: boolean
-  multiple?: boolean
+type SelectProps = ListboxProps & {
+  value?: string | number
+  onChange?: (value: string | number) => void
+  defaultValue?: string | number
+  options: Array<{
+    value: string | number
+    label: string
+  }>
   placeholder?: string
 }
 
+function compareOption(a: any, b: any) {
+  return a.value === b.value
+}
+
 export const Select: FC<SelectProps> = props => {
+  const { value: _, defaultValue: _defaultValue, onChange, options = [], placeholder = '请选择', ...others } = props
   const [value, setValue] = useValue(props)
+  const current = options.find(options => options.value === value)
+  const defaultValue = useMemo(() => options.find(options => options.value === _defaultValue), [_defaultValue, options])
 
   return (
-    <Popper
-      trigger="click"
-      placement="bottom-start"
-      sizeMode="reference-width"
-      offset={10}
-      content={() => {
-        return (
-          <Listbox
-            items={[
-              { value: '1', label: 'Option 1' },
-              { value: '2', label: 'Option 2' },
-              { value: '3', label: 'Option 3' },
-            ]}
-          ></Listbox>
-        )
-      }}
+    <Listbox
+      by={compareOption}
+      value={current}
+      defaultValue={defaultValue}
+      onChange={item => setValue(item.value)}
+      {...others}
     >
-      <InputBox as="button" suffix={<ChevronDownIcon className="mr-1 h-4 w-4 shrink-0" />}>
-        <div
-          className={clsx([
-            'block w-full flex-1 cursor-[inherit] border-0 bg-transparent py-1.5 text-sm leading-snug outline-none focus:outline-none',
-            { 'pl-3': true, 'text-mute': !props.value },
-          ])}
-        >
-          {props.value || props.placeholder}
+      <ListboxButton
+        className={clsx([
+          'bg-input-background border-input-border data-[invalid]:border-danger data-[invalid]:data-[focus]:ring-danger flex h-9 cursor-pointer appearance-none items-center overflow-hidden rounded-md border pl-3 text-left data-[disabled]:cursor-not-allowed data-[disabled]:opacity-60',
+          'data-[focus]:border-primary data-[focus]:ring-primary data-[active]:border-primary data-[active]:ring-primary data-[focus]:ring-1 data-[active]:ring-1',
+        ])}
+      >
+        <div className="flex flex-1 items-center overflow-hidden text-nowrap">
+          {value ? (
+            <span className="w-full text-ellipsis text-nowrap">{value}</span>
+          ) : (
+            <span className="text-mute">{placeholder}</span>
+          )}
         </div>
-      </InputBox>
-    </Popper>
+        <ChevronDownIcon className="text-mute mr-2 h-4 w-4 shrink-0"></ChevronDownIcon>
+      </ListboxButton>
+      <ListboxOptions
+        anchor="bottom start"
+        className={clsx([
+          'ring-line w-[var(--button-width)] rounded p-1 text-sm ring-1 shadow shadow-md [--anchor-gap:var(--spacing-1)] focus:outline-none',
+          'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0',
+        ])}
+      >
+        {options.map(item => (
+          <ListboxOption key={item.value} value={item.value} className="data-[focus]:bg-blue-100">
+            {item.label}
+          </ListboxOption>
+        ))}
+      </ListboxOptions>
+    </Listbox>
   )
 }
