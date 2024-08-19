ae38fb7dbf4c0e2482087b919e3a1480c966796a
JasKang <jaskang@qq.com>
2024-08-19 23:15:59 +0800

    feat: select

--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -1,6 +1,6 @@
-import { type FC, useMemo } from 'react'
+import { type FC } from 'react'
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions, type ListboxProps } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/24/outline'
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/24/outline'
 import { clsx } from 'kotl'
 import useValue from '@/hooks/useValue'
 
@@ -11,28 +11,24 @@ type SelectProps = ListboxProps & {
   options: Array<{
     value: string | number
     label: string
+    disabled?: boolean
   }>
   placeholder?: string
 }
 
-function compareOption(a: any, b: any) {
-  return a.value === b.value
+function compareOption(a: string, b: string) {
+  return a === b
 }
 
 export const Select: FC<SelectProps> = props => {
-  const { value: _, defaultValue: _defaultValue, onChange, options = [], placeholder = '请选择', ...others } = props
-  const [value, setValue] = useValue(props)
+  const { value: _, defaultValue = '', onChange, options = [], placeholder = '请选择', ...others } = props
+  const [value, setValue] = useValue(props, {
+    defaultValue,
+  })
   const current = options.find(options => options.value === value)
-  const defaultValue = useMemo(() => options.find(options => options.value === _defaultValue), [_defaultValue, options])
 
   return (
-    <Listbox
-      by={compareOption}
-      value={current}
-      defaultValue={defaultValue}
-      onChange={item => setValue(item.value)}
-      {...others}
-    >
+    <Listbox by={compareOption} value={value} defaultValue={defaultValue} onChange={val => setValue(val)} {...others}>
       <ListboxButton
         className={clsx([
           'bg-input-background border-input-border data-[invalid]:border-danger data-[invalid]:data-[focus]:ring-danger flex h-9 cursor-pointer appearance-none items-center overflow-hidden rounded-md border pl-3 text-left data-[disabled]:cursor-not-allowed data-[disabled]:opacity-60',
@@ -40,8 +36,8 @@ export const Select: FC<SelectProps> = props => {
         ])}
       >
         <div className="flex flex-1 items-center overflow-hidden text-nowrap">
-          {value ? (
-            <span className="w-full text-ellipsis text-nowrap">{value}</span>
+          {current ? (
+            <span className="w-full text-ellipsis text-nowrap">{current.label}</span>
           ) : (
             <span className="text-mute">{placeholder}</span>
           )}
@@ -51,13 +47,21 @@ export const Select: FC<SelectProps> = props => {
       <ListboxOptions
         anchor="bottom start"
         className={clsx([
-          'ring-line w-[var(--button-width)] rounded p-1 text-sm ring-1 shadow shadow-md [--anchor-gap:var(--spacing-1)] focus:outline-none',
+          'ring-line w-[var(--button-width)] rounded bg-white p-1 text-sm ring-1 shadow shadow-md [--anchor-gap:var(--spacing-1)] focus:outline-none',
           'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0',
         ])}
       >
         {options.map(item => (
-          <ListboxOption key={item.value} value={item.value} className="data-[focus]:bg-blue-100">
-            {item.label}
+          <ListboxOption
+            key={item.value}
+            value={item.value}
+            disabled={item.disabled}
+            className="group data-[focus]:bg-input-background-hover flex min-h-9 w-full items-center gap-1 rounded"
+          >
+            <div className="flex flex-1 items-center overflow-hidden text-nowrap pl-2">
+              <span className="w-full text-ellipsis text-nowrap group-data-[selected]:font-medium">{item.label}</span>
+            </div>
+            <CheckIcon className="invisible mr-2 h-4 w-4 shrink-0 group-data-[selected]:visible" />
           </ListboxOption>
         ))}
       </ListboxOptions>
